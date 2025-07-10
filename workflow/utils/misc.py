f57ca5d449e04d9b3f91a003ae229e2e0a05d3f5
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-07-10 05:30:50 -0700

    trim UMAP when output by rapids_singlecell

--- workflow/utils/misc.py
+++ workflow/utils/misc.py
@@ -24,6 +24,15 @@ def remove_outliers(adata, extrema='max', factor=10, rep='X_umap'):
     return adata[outlier_mask]
 
 
+def trim_umap(embed, old_key='X_umap', new_key='X_umap', threshold=1e-5):
+    """Code provided by @moinfar, modified by @mumichae"""
+    x_min, x_max = np.quantile(embed.obsm[old_key][:, 0], (threshold, 1-threshold))
+    x_min, x_max = float(x_min), float(x_max)
+    y_min, y_max = np.quantile(embed.obsm[old_key][:, 1], (threshold, 1-threshold))
+    y_min, y_max = float(y_min), float(y_max)
+    embed.obsm[new_key] = np.vstack([embed.obsm[new_key][:, 0].clip(x_min, x_max), embed.obsm[old_key][:, 1].clip(y_min, y_max)]).T
+
+
 def all_but(_list, is_not):
     return [x for x in _list if x != is_not]
 
@@ -156,6 +165,9 @@ def ensure_sparse(adata, layers: [str, list] = None, sparse_type=None, **kwargs)
     def to_sparse(matrix, sparse_type=None):
         from scipy.sparse import csr_matrix
         from dask import array as da
+        
+        if matrix is None:
+            return matrix
         
         if sparse_type is None:
             sparse_type = csr_matrix
