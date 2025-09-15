251a5c3444de5b0b0e25a2740cee2bb18bb9eb3e
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-09-15 07:37:39 -0700

    make varm['PCs'] work with non-subset count matrix

--- workflow/preprocessing/scripts/pca.py
+++ workflow/preprocessing/scripts/pca.py
@@ -25,9 +25,12 @@ dask = args.pop('dask', dask) # overwrite with pca-specific dask flag
 logging.info(f'Read "{input_file}"...')
 adata = read_anndata(
     input_file,
+    X='X',
     var='var',
     obs='obs',
     uns='uns',
+    dask=True,
+    backed=True,
 )
 logging.info(adata.__str__())
 
@@ -49,12 +52,11 @@ if adata.n_obs == 0:
     adata.write_zarr(output_file)
     exit(0)
 
-# read matrix
-adata.X = read_anndata(input_file, X='X', dask=True, backed=True).X
-
 logging.info('Subset to highly variable genes...')
 hvg_key = args.pop('mask_var', 'highly_variable')
-adata, _ = subset_hvg(adata, var_column=hvg_key, compute_dask=not dask)
+var = adata.var.copy()
+adata_pca, _ = subset_hvg(adata, var_column=hvg_key, compute_dask=not dask)
+adata = sc.AnnData(obs=adata.obs, var=var)
 
 if USE_GPU:
     sc.get.anndata_to_GPU(adata)
@@ -62,10 +64,22 @@ if USE_GPU:
 # scaling TODO: move to separate rule
 if scale:
     logging.info('Scale data...')
-    sc.pp.scale(adata)
+    sc.pp.scale(adata_pca)
 
 logging.info(f'PCA with parameters: {args}')
-sc.pp.pca(adata, **args)
+sc.pp.pca(adata_pca, **args)
+adata.obsm['X_pca'] = adata_pca.obsm['X_pca']
+adata.uns['pca'] = adata_pca.uns['pca']
+
+# Map all entries in varm back to all genes
+for key, varm in adata_pca.varm.items():
+    full_arr = np.full(
+        (adata.n_vars, varm.shape[1]),
+        np.nan,
+        dtype=varm.dtype
+    )
+    full_arr[adata.var_names.isin(adata_pca.var_names)] = varm
+    adata.varm[key] = full_arr
 
 logging.info(f'Write to {output_file}...')
 logging.info(adata.__str__())
