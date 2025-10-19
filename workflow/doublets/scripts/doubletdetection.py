0e1b57e577fc781df9fbe269a6fb09e5feb5f920
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2025-10-19 15:40:04 +0200

    subset to batches before running doublet methods

--- workflow/doublets/scripts/doubletdetection.py
+++ workflow/doublets/scripts/doubletdetection.py
@@ -57,6 +57,9 @@ adata = read_anndata(
 with open(batches_txt, 'r') as f:
     batches = [line.strip().split('\t')[0] for line in f if line.strip()]
 
+if batch_key is not None:
+    adata = adata[adata.obs[batch_key].isin(batches), :].copy()
+
 logging.info('Run doubletdetection...')
 results = (
     run_method(adata, batch_key, batch)
