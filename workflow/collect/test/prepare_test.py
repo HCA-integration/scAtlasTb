b55f0bbac3c75505bffc813747bfc55ed1de9ae7
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-11-23 16:06:19 -0800

    shuffle modified file

--- workflow/collect/test/prepare_test.py
+++ workflow/collect/test/prepare_test.py
@@ -1,4 +1,5 @@
 import anndata as ad
+import numpy as np
 
 file_path = 'input/pbmc68k.h5ad'
 out_path = 'input/pbmc68k_modified.zarr'
@@ -7,6 +8,11 @@ print(f'Read {file_path}...', flush=True)
 adata = ad.read_h5ad(file_path)
 adata.obs['louvain'] = adata.obs['batch']
 adata.obs['new_column'] = 'for collect module'
+
+# Shuffle cells
+rng = np.random.default_rng(42)
+perm = rng.permutation(adata.n_obs)
+adata = adata[perm].copy()
 
 print(f'Write to {out_path}...', flush=True)
 adata.write_zarr(out_path)
