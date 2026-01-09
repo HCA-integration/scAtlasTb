821884e95ac4e09a8e2c18a3cc007e1f0d36e245
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2026-01-09 15:22:10 -0800

    integrate sex prediction to final object

--- workflow/celltype_prediction/scripts/collect.py
+++ workflow/celltype_prediction/scripts/collect.py
@@ -2,12 +2,18 @@ from utils.io import read_anndata, write_zarr_linked
 
 input_file = snakemake.input[0]
 input_celltypist = snakemake.input.celltypist
+input_predict_sex = snakemake.input.predict_sex
 output_file = snakemake.output[0]
 
 print(f'Read file: {input_file}...', flush=True)
 adata = read_anndata(input_file, obs='obs')
 
 for file in input_celltypist:
+    print(f'Read file: {file}...', flush=True)
+    obs = read_anndata(file, obs='obs').obs
+    adata.obs[obs.columns] = obs
+
+for file in input_predict_sex:
     print(f'Read file: {file}...', flush=True)
     obs = read_anndata(file, obs='obs').obs
     adata.obs[obs.columns] = obs
