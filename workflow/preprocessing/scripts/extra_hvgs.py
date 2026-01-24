c097666d07716e2a533a56717ce43c4ef773ccf7
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2026-01-24 09:00:44 -0800

    remove args==False for extra_hvgs

--- workflow/preprocessing/scripts/extra_hvgs.py
+++ workflow/preprocessing/scripts/extra_hvgs.py
@@ -43,7 +43,7 @@ logging.info(f'Dask enabled: {dask}')
 hvg_column_name = 'extra_hvgs'
 use_gpu = USE_GPU
 
-if args is None:
+if not args:
     args = {}
 elif isinstance(args, dict):
     args.pop('subset', None) # don't support subsetting
@@ -85,110 +85,106 @@ if adata.n_obs == 0:
     adata.write_zarr(output_file)
     exit(0)
 
-if args == False:
-    logging.info('No highly variable gene parameters provided, including all genes...')
-    adata.var[hvg_column_name] = True
-else:
-    var = adata.var.copy()
-    
-    # workaround for CxG datasets
-    feature_col = 'feature_name' if 'feature_name' in var.columns else None
+var = adata.var.copy()
 
-    # remove user-specified genes
-    if remove_genes:
-        remove_genes = match_genes(var, remove_genes, column=feature_col)
-        logging.info(f'Remove {len(remove_genes)} genes (subset data)...')
-        adata = adata[:, ~adata.var_names.isin(remove_genes)].copy()
+# workaround for CxG datasets
+feature_col = 'feature_name' if 'feature_name' in var.columns else None
 
-    adata.var[hvg_column_name] = False
-    
-    if union_over is not None:
-        logging.info(f'Compute highly variable genes per {union_over} with args={args}...')
-        if isinstance(union_over, str):
-            union_over = [union_over]
-        union_values = adata.obs[union_over].astype(str) \
-            .replace(['nan', 'unknown'], np.nan).dropna() \
-            .apply(lambda x: '--'.join(x), axis=1)
-        
-        # remove groups with fewer than min_cells
-        value_counts = union_values.value_counts()
-        remove_groups = value_counts[value_counts < min_cells]
-        union_values = union_values[~union_values.isin(remove_groups.index)]
-        
-        # set union_over values in adata.obs
-        adata.obs['union_over'] = union_values
-        adata.obs['union_over'] = adata.obs['union_over'].astype('category')
-        
-        logging.info(adata.obs['union_over'].value_counts(dropna=False))
-        if len(remove_groups) > 0:
-            logging.info(f'Removed groups with fewer than {min_cells} cells:\n{remove_groups}')
-        
-        for group in tqdm(
-            adata.obs['union_over'].dropna().unique(),
-            miniters=1,
-            desc='Computing HVGs per group',
-        ):
-            _ad = adata[adata.obs['union_over'] == group].copy()
-            
-            if not dask:
-                logging.info('Compute matrix...')
-                _ad = dask_compute(_ad)
-            
-            # filter genes and cells that would break HVG function
-            batch_mask = _filter_batch(_ad, batch_key=args.get('batch_key'))
-            _ad = _ad[batch_mask, _ad.var['nonzero_genes']].copy()
-            
-            # if _ad.n_obs > 1e6:
-            #     use_gpu = False
-            #     sc = scanpy
-            # elif USE_GPU:
-            #     use_gpu = True
-            #     sc = rsc
-            
-            if use_gpu:
-                rsc.get.anndata_to_GPU(_ad)
+# remove user-specified genes
+if remove_genes:
+    remove_genes = match_genes(var, remove_genes, column=feature_col)
+    logging.info(f'Remove {len(remove_genes)} genes (subset data)...')
+    adata = adata[:, ~adata.var_names.isin(remove_genes)].copy()
 
-            sc.pp.highly_variable_genes(_ad, **args)
-            
-            # get union of gene sets
-            adata.var[hvg_column_name] = adata.var[hvg_column_name] | _ad.var['highly_variable']
-            del _ad
-        
-        logging.info(f'Computed {adata.var[hvg_column_name].sum()} highly variable genes.')
+adata.var[hvg_column_name] = False
+
+if union_over is not None:
+    logging.info(f'Compute highly variable genes per {union_over} with args={args}...')
+    if isinstance(union_over, str):
+        union_over = [union_over]
+    union_values = adata.obs[union_over].astype(str) \
+        .replace(['nan', 'unknown'], np.nan).dropna() \
+        .apply(lambda x: '--'.join(x), axis=1)
     
-    else:
-        # default gene selection
-        logging.info(f'Select features for all cells with arguments: {args}...')
-        if use_gpu:
-            sc.get.anndata_to_GPU(adata)
+    # remove groups with fewer than min_cells
+    value_counts = union_values.value_counts()
+    remove_groups = value_counts[value_counts < min_cells]
+    union_values = union_values[~union_values.isin(remove_groups.index)]
+    
+    # set union_over values in adata.obs
+    adata.obs['union_over'] = union_values
+    adata.obs['union_over'] = adata.obs['union_over'].astype('category')
+    
+    logging.info(adata.obs['union_over'].value_counts(dropna=False))
+    if len(remove_groups) > 0:
+        logging.info(f'Removed groups with fewer than {min_cells} cells:\n{remove_groups}')
+    
+    for group in tqdm(
+        adata.obs['union_over'].dropna().unique(),
+        miniters=1,
+        desc='Computing HVGs per group',
+    ):
+        _ad = adata[adata.obs['union_over'] == group].copy()
         
         if not dask:
             logging.info('Compute matrix...')
-            adata = dask_compute(adata)
-
-        with TqdmCallback(desc=f'Select features with arguments: {args}...', miniters=1):
-            sc.pp.highly_variable_genes(adata, **args)
-        adata.var[hvg_column_name] = adata.var['highly_variable']
-
-    # set extra_hvgs in full dataset
-    var[hvg_column_name] = False
-    var.loc[adata.var_names, hvg_column_name] = adata.var[hvg_column_name]
-
-    # add user-provided genes
-    if extra_genes:
-        n_genes = len(extra_genes)
-        extra_genes = match_genes(var, extra_genes, column=feature_col)
+            _ad = dask_compute(_ad)
         
-        if len(extra_genes) < n_genes:
-            logging.warning(f'Only {len(extra_genes)} of {n_genes} user-provided genes found in data...')
-        if len(extra_genes) == 0:
-            logging.info('No extra user genes added...')
-        else:
-            logging.info(f'Add {len(extra_genes)} user-provided genes...')
-            var.loc[extra_genes, hvg_column_name] = True
+        # filter genes and cells that would break HVG function
+        batch_mask = _filter_batch(_ad, batch_key=args.get('batch_key'))
+        _ad = _ad[batch_mask, _ad.var['nonzero_genes']].copy()
+        
+        # if _ad.n_obs > 1e6:
+        #     use_gpu = False
+        #     sc = scanpy
+        # elif USE_GPU:
+        #     use_gpu = True
+        #     sc = rsc
+        
+        if use_gpu:
+            rsc.get.anndata_to_GPU(_ad)
+
+        sc.pp.highly_variable_genes(_ad, **args)
+        
+        # get union of gene sets
+        adata.var[hvg_column_name] = adata.var[hvg_column_name] | _ad.var['highly_variable']
+        del _ad
     
-    # recreate AnnData object for full feature space
-    adata = ad.AnnData(var=var, uns=adata.uns)
+    logging.info(f'Computed {adata.var[hvg_column_name].sum()} highly variable genes.')
+
+else:
+    # default gene selection
+    logging.info(f'Select features for all cells with arguments: {args}...')
+    if use_gpu:
+        sc.get.anndata_to_GPU(adata)
+    
+    if not dask:
+        logging.info('Compute matrix...')
+        adata = dask_compute(adata)
+
+    with TqdmCallback(desc=f'Select features with arguments: {args}...', miniters=1):
+        sc.pp.highly_variable_genes(adata, **args)
+    adata.var[hvg_column_name] = adata.var['highly_variable']
+
+# set extra_hvgs in full dataset
+var[hvg_column_name] = False
+var.loc[adata.var_names, hvg_column_name] = adata.var[hvg_column_name]
+
+# add user-provided genes
+if extra_genes:
+    n_genes = len(extra_genes)
+    extra_genes = match_genes(var, extra_genes, column=feature_col)
+    
+    if len(extra_genes) < n_genes:
+        logging.warning(f'Only {len(extra_genes)} of {n_genes} user-provided genes found in data...')
+    if len(extra_genes) == 0:
+        logging.info('No extra user genes added...')
+    else:
+        logging.info(f'Add {len(extra_genes)} user-provided genes...')
+        var.loc[extra_genes, hvg_column_name] = True
+
+# recreate AnnData object for full feature space
+adata = ad.AnnData(var=var, uns=adata.uns)
 
 logging.info(f'Write to {output_file}...')
 write_zarr_linked(
